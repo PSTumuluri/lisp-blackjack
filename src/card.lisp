bf2f3306fcd631b91266b5f2ead0e0ee0545fe28
Pavan Tumuluri <pst15635@uga.edu>
2022-12-03 13:40:57 -0500

    Implement card colors as a function of suit.
    The assignment to color takes place in the `initialize-instance :after`
    method. Loose coupling of suit from color makes it easy to extend this
    scheme to cards like jokers that have no suit but still have a color.

--- src/card.lisp
+++ src/card.lisp
@@ -1,18 +1,29 @@
-(defvar *ranks* '('ace 'two 'three 'four 'five 'six 'seven 'eight 'nine 'ten 'jack 'queen 'king))
-(defvar *suits* '('spade 'club 'diamond 'heart))
+(defvar *ranks* '(ace two three four five six seven eight nine ten jack queen king))
+(defvar *suits* '(spade club diamond heart))
 
 (defclass playing-card ()
   ((rank
     :initarg :rank
     :reader rank
-    :documentation "The card's rank, a member of {'ace, 'two, ..., 'king}.")
+    :documentation "The card's rank, a member of {ace, two, ..., king}.")
    (suit
     :initarg :suit
     :reader suit
-    :documentation "The card's suit, a member of {'spade, 'club, 'diamond, 'heart}."))
+    :documentation "The card's suit, a member of {spade, club, diamond, heart}.")
+   (color
+    :reader color
+    :documentation "The card's color, a member of {black, red}."))
   (:documentation "A card of the playing variety."))
 
+(defmethod initialize-instance :after ((card playing-card) &key)
+  "Sets the color of the card based on its suit."
+  (let ((suit (suit card)))
+    (setf (slot-value card 'color)
+          (cond ((or (eql suit 'spade) (eql suit 'club)) 'black)
+                ((or (eql suit 'heart) (eql suit 'diamond)) 'red)))))
+
 (defun make-deck ()
+  "Makes an unshuffled vector of the 52 standard playing cards."
   (let* ((num-cards (* (length *suits*) (length *ranks*)))
          (deck (make-array num-cards :fill-pointer 0)))
     (dolist (suit *suits*)
